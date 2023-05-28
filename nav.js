72535f933299e3f7051e99c0bec95ecf6ce684fd
alexmalone1 <41651549+alexmalone1@users.noreply.github.com>
2023-05-28 12:07:03 +0100

    update

--- nav.js
+++ nav.js
@@ -7,8 +7,8 @@ navToggle.addEventListener("click", () => {
     const visibility = primaryNav.getAttribute('data-visible');
 
     if (visibility === "false") {
-       primaryNav.setAttribute('data-visible', true);
-       navToggle.setAttribute('aria-expanded', true);
+        primaryNav.setAttribute('data-visible', true);
+        navToggle.setAttribute('aria-expanded', true);
     } else if (visibility === "true") {
         primaryNav.setAttribute('data-visible', false);
         navToggle.setAttribute('aria-expanded', false);
